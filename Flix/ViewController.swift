1b14b7e2a27dfc3cee197ce830b045b0ddfe5f5e
Rachel Sacdalan <rachelsacdalan@Rachels-MacBook-Air.local>
2022-01-23 00:41:26 -0500

    Created Flix

---
+++ Flix/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Flix
+//
+//  Created by Rachel Sacdalan on 1/22/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
