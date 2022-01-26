3cfe1fc06086f66d1e0491861482b438317d9c8f
Rachel Sacdalan <rachelsacdalan@Rachels-MacBook-Air.local>
2022-01-26 03:47:28 -0500

    Added Movie Grid / Superhero Tab

---
+++ Flix/MovieGridCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  MovieGridCollectionViewCell.swift
+//  Flix
+//
+//  Created by Rachel Sacdalan on 1/26/22.
+//
+
+import UIKit
+
+class MovieGridCollectionViewCell: UICollectionViewCell {
+    @IBOutlet weak var posterImageView: UIImageView!
+    
+}
